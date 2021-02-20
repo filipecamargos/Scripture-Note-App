4e12663abb52020fb5d3e587a7a05c55da202193
filipecamargos <filipe.atrabalho@gmail.com>
2021-02-20 11:19:50 -0700

    Model added

---
+++ Models/Scripture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyScriptureJournal.Models
+{
+    //This is the model for the scripture note to be manipulated
+    public class Scripture
+    {   
+        //The scripture ID
+        public int ID { get; set; }
+
+        //Scritpure reference
+        public string MyScripture { get; set; }
+
+        //Date of entry
+        [DataType(DataType.Date)]
+        public DateTime AddedDate { get; set; }
+        
+        //Scripture Note
+        public string SriptureNote { get; set; }
+
+
+
+    }
+}
